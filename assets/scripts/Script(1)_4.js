8ca2e296c4de4530af310418030411aa0a9c0daf
lazarospapanikolaou <44327139+lazarospapanikolaou@users.noreply.github.com>
2018-12-19 20:06:24 +0200

    Update Script(1)_4.js

--- assets/scripts/Script(1)_4.js
+++ assets/scripts/Script(1)_4.js
@@ -9,11 +9,11 @@ var radius = ((Math.min((width - margin), (height - margin)) / 2) * 0.9  ),
 
 var data = [
 
-      {number: "Ρωμαιοκαθολικοί", rate: 1,600,000,000},
-      {number: "Ισλαμιστές", rate: 500,000,000},
-      {number: "Προτεστάντες", rate: 900,000,000},
-      {number: "Άθεοι", rate: 2,800,000,000},
-      {number: "Άλλες Θρησκείες", rate: 150,000,000}
+      {number: "Ρωμαιοκαθολικοί", rate: 538},
+      {number: "Ισλαμιστές", rate: 613},
+      {number: "Προτεστάντες", rate: 456},
+      {number: "Άθεοι", rate: 345},
+      		{number: "Άλλες Θρησκείες", rate: 278}
       
     ];
 
